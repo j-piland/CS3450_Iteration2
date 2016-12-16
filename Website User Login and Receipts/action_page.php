b2787ea3cf92142e7a8e7d326e1dec42ad8f8f65
spencerduff <spencer.duff@gmail.com>
2016-12-15 18:28:04 -0700

    Add files via upload

---
+++ Website User Login and Receipts/action_page.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["eml"]; ?>
+Your address is <?php echo $_POST["add"]; ?>
+Your phone is <?php echo $_POST["phn"]; ?>
+
+</body>
+</html>
